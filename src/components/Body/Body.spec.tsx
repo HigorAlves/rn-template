5d9e039fd181333996c5a0ec09e3f87203bfccaf
Higor Alves <higorhaalves@gmail.com>
2020-08-08 17:11:30 -0300

    feat(test): code coverage with reports
    closes #15

--- src/components/Body/Body.spec.tsx
+++ src/components/Body/Body.spec.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-import App from '@/App';
+import App from './Body';
 
 it('renders correctly', () => {
 	renderer.create(<App />);
